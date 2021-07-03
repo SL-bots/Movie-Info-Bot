f66e748e550e4bb090b4857810cefbc4a63b2ce5
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-07-03 14:59:17 +0530

    Update info.py

--- plugins/info/info.py
+++ plugins/info/info.py
@@ -6,3 +6,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 @Client.on_message(filters.command(["info", "information"]))
 async def get_command(bot, update):
     movie = update.text.split(" ", 1)
+
+@Client.on_message(filters.private & filters.text)
+async def get_movie(bot, update):
+    movie = update.text
