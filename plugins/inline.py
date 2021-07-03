dfc0203818eaaadcc0ffe1aef8ab01c3e5f2c052
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-07-03 22:04:42 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -13,10 +13,19 @@ async def inline_info(bot, update):
         number, movie_api = update.data.split("+", -1)
         r = requests.get(movie_api)
         movies = r.json()
-        answers = []
-        try:
-            movie = movies[number-1]
-            movie_info = info(movie)
-        except Exception as error:
-            print(error)
-            
+        movie = movies[number-1]
+        movie_info = info(movie)
+        answers = [
+            InlineQueryResultArticle(
+                title=movie['title'],
+                description=f"{movie['title']} - {movie['type']} - {movie['year']}",
+                input_message_content=InputTextMessageContent(
+                    text=movie_info,
+                    disable_web_page_preview=True
+                )
+            )
+        ]
+    await bot.answer_inline_query(
+        inline_query_id=update.chat.id,
+        results=answers
+    )
