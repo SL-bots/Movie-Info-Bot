3b36c35849a08ac94ad0a39ce7ef293e6c4939d3
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-07-03 19:17:48 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1,6 +1,7 @@
 # Author: Fayas (https://github.com/FayasNoushad) (@FayasNoushad)
 
 from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from plugins.info import *
 
 
