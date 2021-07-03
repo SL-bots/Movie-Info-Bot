5fcf525c54f6243cdeadf74598c59d244da97c38
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-07-03 17:09:25 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -1,6 +1,8 @@
 # Author: Fayas (https://github.com/FayasNoushad) (@FayasNoushad)
 
 from pyrogram import Client
+from plugins.info import cb_edit
+
 
 @Client.on_callback_query()
 async def callback(bot, update):
