c4a81ee1316efb257836f34f1d245013b90d4493
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-07-03 16:17:29 +0530

    Update __init__.py

--- plugins/info/__init__.py
+++ plugins/info/__init__.py
@@ -74,13 +74,15 @@ def info(movie):
     info = f"Title: {movie['title']}\n"
     info += f"Type: {movie['type']}\n"
     if movie['providers']:
-        providers = movie['providers']
-        info += f"Providers:"
         try:
+            providers = movie['providers']
+            info += f"Providers:"
             providers = movie['providers']
             for provider in providers:
                 info += f" [{provider}]({providers[provider]})"
             info += "\n"
+        except:
+            pass
     try:
         info += f"Release Date: {str(movie['release_date'])}\n"
     except:
