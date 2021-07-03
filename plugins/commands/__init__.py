c97553fd397ef711eaf2035cd2c5f53c9fbf12bd
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-07-03 15:45:03 +0530

    Update __init__.py

--- plugins/commands/__init__.py
+++ plugins/commands/__init__.py
@@ -6,7 +6,7 @@ from pyrogram import Client, filters
 @Client.on_message(filters.private & filters.command(["start"]))
 async def start(bot, update):
     if update.text == "/start":
-        pass
+        await update.reply_text(f"Hi {update.from_user.mention}")
     else:
         movie = update.text.split(" ", 1)
         await get_movie(bot, update.message, movie)
