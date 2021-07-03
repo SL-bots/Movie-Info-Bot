b4a54528c6d42f128ccaec50317eaff21b013562
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-07-03 14:34:52 +0530

    Update __init__.py

--- plugins/info/__init__.py
+++ plugins/info/__init__.py
@@ -4,3 +4,33 @@ import requests
 
 
 API = 'https://api.sumanjay.cf/watch/'
+
+
+def info(movie):
+    info = f"Title: {movie['title']}\n"
+    info += f"Type: {movie['type']}\n"
+    if movie['providers']:
+        providers = movie['providers']
+        info += f"Providers:"
+        try:
+            providers = movie['providers']
+            for provider in providers:
+                info += f" [{provider}]({providers[provider]})"
+            info += "\n"
+    try:
+        info += f"Release Date: {str(movie['release_date'])}\n"
+    except:
+        pass
+    try:
+        info += f"Release Year: {movie['release_year']}\n"
+    except:
+        pass
+    try:
+        if movie['score']:
+            scores = movie['score']
+            info += "Score:"
+            for score in scores:
+                info += f" {score} - {str(scores[score])}"
+    except:
+        pass
+    return info
