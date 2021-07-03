7d5e6745f9f7895d98f6f0b4e8d26e5ef4d68935
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-07-03 16:59:26 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -4,7 +4,7 @@ from pyrogram import Client
 
 @Client.on_callback_query()
 async def callback(bot, update):
-    if update.query.startswith("movie+"):
+    if update.data.startswith("movie+"):
         data = update.data.split("movie+", 1)
         movie, type = data.data.split("+", 1)
         await cb_edit(bot, update.message, movie, type)
