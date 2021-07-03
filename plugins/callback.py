bd786d64a81e92b505c1418827b139b830b690d3
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-07-03 19:20:58 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -8,5 +8,5 @@ from plugins.info import *
 @Client.on_callback_query()
 async def callback(bot, update):
     if update.data.startswith("movie+"):
-        movie, type, year = update.data.split("+", 3)
+        startwidth, movie, type, year = update.data.split("+", 3)
         await cb_edit(bot, update.message, movie, type, year)
