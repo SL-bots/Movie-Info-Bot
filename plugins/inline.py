6d877bdeda51eb202671362eed718860b6bcc581
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-07-03 22:11:52 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -9,8 +9,8 @@ from plugins.info import *
 
 @Client.on_inline_query()
 async def inline_info(bot, update):
-        data = update.data
-        number, movie_api = update.data.split("+", -1)
+        query = update.query
+        number, movie_api = query.split("+", -1)
         r = requests.get(movie_api)
         movies = r.json()
         movie = movies[number-1]
