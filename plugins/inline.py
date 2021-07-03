44a88c7bece603fcadffaa5b6e533b4983aed08b
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-07-03 21:55:51 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -1,6 +1,22 @@
 # Author: Fayas (https://github.com/FayasNoushad) (@FayasNoushad)
 
 
+imported
 from pyrogram import Client, filters 
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from plugins.info import *
+
+
+@Client.on_inline_query()
+async def inline_info(bot, update):
+        data = update.data
+        number, movie_api = update.data.split("+", -1)
+        r = requests.get(movie_api)
+        movies = r.json()
+        answers = []
+        try:
+            movie = movies[number-1]
+            movie_info = info(movie)
+        except Exception as error:
+            print(error)
+            
