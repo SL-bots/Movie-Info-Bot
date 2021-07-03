4a0b70f53b6850e60007b355c2048872a61c6f0e
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-07-03 15:44:20 +0530

    Update __init__.py

--- plugins/commands/__init__.py
+++ plugins/commands/__init__.py
@@ -1,3 +1,12 @@
 # Author: Fayas (https://github.com/FayasNoushad) (@FayasNoushad)
 
 from pyrogram import Client, filters
+
+
+@Client.on_message(filters.private & filters.command(["start"]))
+async def start(bot, update):
+    if update.text == "/start":
+        pass
+    else:
+        movie = update.text.split(" ", 1)
+        await get_movie(bot, update.message, movie)
