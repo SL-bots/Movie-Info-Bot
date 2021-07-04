55e4524fe92cbe2ea509c4a0ee4f3cc98ebb1854
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-07-04 11:57:31 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -15,15 +15,19 @@ async def inline_info(bot, update):
         r = requests.get(API + movie_name)
         movies = r.json()
         movie = movies[number]
+        photo = thumb(movie)
         movie_info = info(movie)
+        providers = providers(movie) if movie['providers'] else []
         answers = [
             InlineQueryResultArticle(
                 title=movie['title'],
+                thumb_url=photo,
                 description=f"{movie['title']} - {movie['type']} - {movie['release_year']}",
                 input_message_content=InputTextMessageContent(
                     message_text=movie_info,
                     disable_web_page_preview=True
                 )
+                reply_markup=InlineKeyboardMarkup([providers])
             )
         ]
         await bot.answer_inline_query(
