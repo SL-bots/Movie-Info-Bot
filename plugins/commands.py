fd80f72e200247d554b2ec98fd7e13fb163d3d99
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-08-04 16:22:08 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -15,7 +15,11 @@ BUTTONS = InlineKeyboardMarkup(
 @Client.on_message(filters.private & filters.command(["start"]), group=-1)
 async def start(bot, update):
     if update.text == "/start":
-        await update.reply_text(f"Hi {update.from_user.mention},\nSend me a movie name")
+        await update.reply_text(
+            text=f"Hi {update.from_user.mention},\nSend me a movie name",
+            reply_markup=BUTTONS,
+            quote=True
+        )
     else:
         movie = update.text.split(" ", 1)
         await get_movie(bot, update, movie)
